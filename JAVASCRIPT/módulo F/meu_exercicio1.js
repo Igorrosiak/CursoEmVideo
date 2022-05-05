7edd84e41994f40ffce841d2c1c3aec0483c82eb
Igorrosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-05-05 01:07:50 -0300

    finalizando curso JS

--- JAVASCRIPT/módulo F/meu_exercicio1.js
+++ JAVASCRIPT/módulo F/meu_exercicio1.js
@@ -6,6 +6,7 @@ function Adicionar(){
     tex.innerHTML = 'Iniciando Array <br>'
 
     let num = Number(document.getElementById('tab').value)
+    
     array1.push(num)
 
     for(let i in array1){
@@ -20,7 +21,7 @@ function Finalizar(){
     tex2.innerHTML = `Ao todo temos ${array1.length} números cadastrados <br> <br>
     O maior valor informado foi ${array1.sort()[array1.length-1]} <br> <br> 
     O menor valor informado foi ${array1.sort()[array1[0]]} <br> <br>
-    Somando todos os valores temos 
+    Somando todos os valores temos ${array1}
     A média dos valores digitados é `
 
 }
